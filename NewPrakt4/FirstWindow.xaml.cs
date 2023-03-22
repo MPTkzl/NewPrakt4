1299e465759238753f7a50fb18987010f7069106
MPTkzl <105377542+yye1337@users.noreply.github.com>
2023-03-22 10:26:16 +0300

    123

---
+++ NewPrakt4/FirstWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using NewPrakt4.qqqDataSetTableAdapters;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NewPrakt4
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        zakaziTableAdapter zakazi = new zakaziTableAdapter();
+        
+        public FirstWindow()
+        {
+            InitializeComponent();
+            Zakazi.ItemsSource = zakazi.GetData();
+            
+        }
+        
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            object id = (Zakazi.SelectedItem as DataRowView).Row[0];
+            zakazi.UpdateQuery(Convert.ToInt32(NameTbx.Text), Convert.ToInt32(NameTbx1.Text), Convert.ToInt32(NameTbx2.Text), Convert.ToInt32(id));
+            Zakazi.ItemsSource = zakazi.GetData();
+        }
+
+        private void Zakazi_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Zakazi.SelectedItem as DataRowView != null)
+            {
+                NameTbx.Text = Convert.ToString((Zakazi.SelectedItem as DataRowView).Row[0]);
+                NameTbx1.Text = Convert.ToString((Zakazi.SelectedItem as DataRowView).Row[1]);
+                NameTbx2.Text = Convert.ToString((Zakazi.SelectedItem as DataRowView).Row[2]);
+            }
+        }
+    }
+}
---
+++ NewPrakt4/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewPrakt4.qqqDataSetTableAdapters;
+
+namespace NewPrakt4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        usersTableAdapter users = new usersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Users.ItemsSource = users.GetData();
+        }
+
+       
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            FirstWindow window1 = new FirstWindow();
+            window1.ShowDialog();
+        }
+
+       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            object id = (Users.SelectedItem as DataRowView).Row[0];
+            users.UpdateQuery(Convert.ToInt32(NameTbx.Text), (NameTbx1.Text), (NameTbx2.Text), Convert.ToInt32(id));
+            Users.ItemsSource = users.GetData();
+        }
+        private void Users_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Users.SelectedItem as DataRowView != null)
+            {
+                NameTbx.Text = Convert.ToString((Users.SelectedItem as DataRowView).Row[0]);
+                NameTbx1.Text = Convert.ToString((Users.SelectedItem as DataRowView).Row[1]);
+                NameTbx2.Text = Convert.ToString((Users.SelectedItem as DataRowView).Row[2]);
+            }
+        }
+
+        
+    }
+}
